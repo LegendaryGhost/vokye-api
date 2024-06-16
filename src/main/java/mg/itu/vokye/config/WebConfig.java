5bc6b45c907b0a8feefc093e5437127068d4b630
tsoa <henintsoapaul@gmail.com>
2024-06-16 16:01:55 +0300

    Allow queries from the Angular app.

---
+++ src/main/java/mg/itu/vokye/config/WebConfig.java
@@ -0,0 +1,17 @@
+package mg.itu.vokye.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+               .allowedOrigins("http://localhost:4200") // Allow requests from Angular app
+               .allowedMethods("*");
+    }
+}
+
