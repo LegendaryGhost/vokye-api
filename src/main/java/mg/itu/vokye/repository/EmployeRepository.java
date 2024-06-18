4f85ba00c852273c0ae76086fd692cea5226b3dc
Hery0019 <herakotonarivo@gmail.com>
2024-06-18 13:27:34 +0300

    Update EmployeRepository.java

--- src/main/java/mg/itu/vokye/repository/EmployeRepository.java
+++ src/main/java/mg/itu/vokye/repository/EmployeRepository.java
@@ -2,5 +2,10 @@ package mg.itu.vokye.repository;
 
 import mg.itu.vokye.entity.Employe;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface EmployeRepository extends JpaRepository<Employe, Long> {}
+public interface EmployeRepository extends JpaRepository<Employe, Long> {
+    @Query("SELECT CASE WHEN COUNT(e) > 0 THEN true ELSE false END FROM Employe e WHERE e.email = :email AND e.mot_de_passe = digest(:motDePasse, 'sha1')")
+    boolean existsByEmailAndMotDePasse(@Param("email") String email, @Param("motDePasse") String motDePasse);
+}
