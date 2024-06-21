4fa794329446d22f30b65e36878efb863bfec806
Hery0019 <herakotonarivo@gmail.com>
2024-06-21 18:20:22 +0300

    Update EmployeRepository.java

--- src/main/java/mg/itu/vokye/repository/EmployeRepository.java
+++ src/main/java/mg/itu/vokye/repository/EmployeRepository.java
@@ -7,11 +7,18 @@ import org.springframework.data.repository.query.Param;
 
 public interface EmployeRepository extends JpaRepository<Employe, Long> {
 
-    @Query(value = "SELECT e.* " +
-            "FROM employe e " +
-            "INNER JOIN type_employe te ON e.id_type_employe = te.id_type_employe " +
-            "WHERE te.designation = 'Manager' " +
-            "AND e.mot_de_passe = crypt(:motDePasse, e.mot_de_passe) " +
-            "AND e.email = :email", nativeQuery = true)
-    Employe existsByEmailAndMotDePasse(@Param("email") String email, @Param("motDePasse") String motDePasse);
+        @Query(value = "SELECT e.* " +
+                        "FROM employe e " +
+                        "INNER JOIN type_employe te ON e.id_type_employe = te.id_type_employe " +
+                        "WHERE te.designation = 'Manager' " +
+                        "AND e.mot_de_passe = crypt(:motDePasse, e.mot_de_passe) " +
+                        "AND e.email = :email", nativeQuery = true)
+        Employe existsByEmailAndMotDePasse(@Param("email") String email, @Param("motDePasse") String motDePasse);
+
+        // @SuppressWarnings("unchecked")
+        // @Query(value = "INSERT INTO employe VALUE " +
+        // "(default, :idEmploye, :nom, :prenom, :email, crypt(:motDePasse,
+        // gen_salt('bf')), :dateEntree, :dateFin)")
+        // Employe save(Employe e);
+
 }
