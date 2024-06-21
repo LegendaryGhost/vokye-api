b0ecc1f9ba0b318bcd9fcf860bf8b53f5942459b
kenny516 <126701465+kenny516@users.noreply.github.com>
2024-06-22 00:15:02 +0300

    Update DepenseRepository.java

--- src/main/java/mg/itu/vokye/repository/DepenseRepository.java
+++ src/main/java/mg/itu/vokye/repository/DepenseRepository.java
@@ -6,20 +6,19 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDate;
-import java.util.Optional;
 
-public interface DepenseRepository extends JpaRepository<Depense, Integer> {
+public interface DepenseRepository extends JpaRepository<Depense,Integer> {
         @Query(value = "SELECT sum(prix) as sum " +
-                        "FROM depense " +
-                        "WHERE (cast(:dateDepense as date) IS NULL OR date_depense = cast(:dateDepense as date)) ", nativeQuery = true)
+                "FROM depense " +
+                "WHERE (cast(:dateDepense as date) IS NULL OR date_depense = cast(:dateDepense as date)) ",
+                nativeQuery = true)
         Double getDepenseAll(@Param("dateDepense") LocalDate dateDepense);
 
         @Query(value = "SELECT sum(prix) as sum " +
-                        "FROM depense " +
-                        "WHERE (extract('month',date_depense) = :month AND extract('year',date_depense) =" +
-                        "" +
-                        " extract('year',cast(:dateDepense as date))) ", nativeQuery = true)
-        Double getDepenseByMonth(@Param("month") Integer month, @Param("year") Integer year);
-
-        Optional<Depense> findById(Long idDepense);
+                "FROM depense " +
+                "WHERE (extract('month',date_depense) = :month AND extract('year',date_depense) =" +
+                "" +
+                " extract('year',cast(:dateDepense as date))) ",
+                nativeQuery = true)
+        Double getDepenseByMonth(@Param("month") Integer month,@Param("year") Integer year);
 }
