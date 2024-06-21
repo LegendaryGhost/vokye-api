a174852c05aa8ea12ca665fd9e9d726c66680a27
Rihantiana Mbolatsiory <108069670+LegendaryGhost@users.noreply.github.com>
2024-06-21 21:24:28 +0300

    Effacement du stock ingredient

--- src/main/java/mg/itu/vokye/repository/StockIngredientRepository.java
+++
@@ -1,7 +0,0 @@
-package mg.itu.vokye.repository;
-
-import mg.itu.vokye.entity.StockIngredient;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StockIngredientRepository extends JpaRepository<StockIngredient, Long> {
-}
