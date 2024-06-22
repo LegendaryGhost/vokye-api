e361974265bdfe10b78bbafa00f3530b7293c991
kenny516 <126701465+kenny516@users.noreply.github.com>
2024-06-22 09:14:03 +0300

    depense et benefice resolved

--- src/main/java/mg/itu/vokye/repository/DepenseRepository.java
+++ src/main/java/mg/itu/vokye/repository/DepenseRepository.java
@@ -10,7 +10,7 @@ import java.time.LocalDate;
 public interface DepenseRepository extends JpaRepository<Depense,Integer> {
         @Query(value = "SELECT COALESCE(sum(prix), 0) as sum " +
                 "FROM depense " +
-                "WHERE (:dateDepense IS NULL OR date_depense = cast(:dateDepense as date))",
+                "WHERE (cast(:dateDepense as date) IS NULL OR date_depense = cast(:dateDepense as date))",
                 nativeQuery = true)
         Double getDepenseAll(@Param("dateDepense") LocalDate dateDepense);
 
